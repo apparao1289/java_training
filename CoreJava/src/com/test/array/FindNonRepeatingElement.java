980c6c995ec8aa121a40b94487847cd326601d0c
apparao <apparao1289@gmail.com>
2020-06-24 10:26:23 +0530

    Added some array problems

--- CoreJava/src/com/test/array/FindNonRepeatingElement.java
+++ CoreJava/src/com/test/array/FindNonRepeatingElement.java
@@ -1,16 +1,25 @@
 package com.test.array;
 
 public class FindNonRepeatingElement {
+	
+	public static void main(String args[]) {
+		// a = {2,3,4,4,3,4,2}  o/p : 2    2^3^4^4^3 = 2^(3^3)^(4^4)      X^X = 0 
+		int a[] = {2,3,4,4,3,2,4};
+		nonRepeatingNumber(a);
+	}
 
-	public static void main(String[] args) {
-		// Find the non repeating number in a given array
-		int a[] = {2,3,4,3,2};  // xor operator  
+	/**
+	 * This method find the non repeating number in given array.
+	 * 
+	 * @param a
+	 */
+	public static void nonRepeatingNumber(int a[]) {
+
 		int result = a[0];
-		for(int i=1;i<a.length;i++) {
+		for (int i = 1; i < a.length; i++) {
 			result = result ^ a[i];
 		}
 		System.out.println(result);
 	}
-
-	
 }
+
